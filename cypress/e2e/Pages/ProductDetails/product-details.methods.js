ed0db098202062c410dce891ec9e04e21686e017
DaMoNBVR <baironvargas0@gmail.com>
2025-05-13 09:15:04 -0600

    Product details Page

---
+++ cypress/e2e/Pages/ProductDetails/product-details.methods.js
@@ -0,0 +1,9 @@
+import { ProductDetailsElements } from "./product-details.elements";
+
+export class ProductDetailsMethods {
+
+    static clickAddToCart() {
+        ProductDetailsElements.buttons.addToCart.click();
+    }
+
+}
--- cypress/e2e/Tests/catalogo-y-compras.cy.js
+++ cypress/e2e/Tests/catalogo-y-compras.cy.js
@@ -32,4 +32,37 @@ describe(CommonData.testSuites.catalogoYCompra, () => {
 
     })
 
+        it ("Agregar Producto al carrito", () => {
+    
+        Logger.StepNumber(1);
+        Logger.Step('Iniciar sesión como usuario registrado');
+        Logger.Substeps('Navegar la página Demoblaze');
+        CommonMethods.NavigateHomePage();
+        Logger.Substeps('Clickear el botón "Log in"');
+        CommonMethods.clickLoginButton();
+        Logger.Substeps('Insertar nombre de usuario y contraseña');
+        LoginMethods.login(CommonData.testData.existingUser, CommonData.testData.existingPassword);
+
+        Logger.StepNumber(2);
+        Logger.Step('Navegar en la página de inicio');
+        Logger.Substeps('Clickear el botón "Home"');
+        CommonMethods.clickHomeButton();
+
+        Logger.StepNumber(3);
+        Logger.Step('Seleccionar una categoría');
+        Logger.Substeps('Clickear el botón "Monitors"');
+        HomeMethods.clickMonitorsOption();
+
+        Logger.Subverification('Verificar que la categoría "Monitors" esté seleccionada');
+        HomeMethods.verifyProductPage('Apple monitor 24');
+        HomeMethods.verifyProductPage('ASUS Full HD');
+
+        Logger.StepNumber(4);
+        HomeMethods.clickProduct('Apple monitor 24');
+
+        Logger.Subverification('Verificar que se muestra la página del producto "Apple monitor 24"');
+        
+
+    })
+
 });
